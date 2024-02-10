d3235bc3ba23cbb2843981a8e73590cd9a3d8639
JiapeiwCMU <jiapeiw@andrew.cmu.edu>
2024-02-10 10:33:14 -0500

    Create script.js

---
+++ solution-hw3/script.js
@@ -0,0 +1,49 @@
+let glazingOptions = [
+    {"Keep original":0},
+    {"Sugar milk":0},
+    {"Vanilla milk":0.5},
+    {"Double chocolate":1.5}
+]
+
+let priceOptions = [
+    {"1":1},
+    {"3":3},
+    {"6":5},
+    {"12":10}
+]
+
+let originalPrice = document.querySelector(".price")
+
+
+// let glazingValue = glazingDropdown.value
+// let packValue = packDropdown.value
+
+
+// let glazingPrice = glazinOptions.glazingValue
+// let packSize = priceOptions.packValue
+
+// console.log("total:",(originalPrice+glazingPrice)*packSize)
+
+// function displayPrice(price){
+
+// }
+function displayPrice(glazing,size){
+    // Display the price after calculation
+    // querySelector to retrieve the price
+    let finalPrice = (originalPrice+glazingOptions.glazing)*priceOptions.size
+
+
+}
+
+function onSelectValueChange(){
+    // get the current information from selection
+    // call displayPrice with the index
+
+    let selectionIndex = parseInt(this.value)
+    let 
+}
+
+let selectElement = document.querySelector(".selection-glazing")
+
+selectElement.addEventListener('change',onSelectValueChange)
+
