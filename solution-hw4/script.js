cfea1cac37dd515a97882396cc689e103db3f0e8
JiapeiwCMU <jiapeiw@andrew.cmu.edu>
2024-02-18 15:02:58 -0500

    Update script.js

--- solution-hw4/script.js
+++ solution-hw4/script.js
@@ -1,23 +1,55 @@
+const rollsData = {
+    "Original": {
+        "basePrice": 2.49,
+        "imageFile": "original-cinnamon-roll.jpg"
+    },
+    "Apple": {
+        "basePrice": 3.49,
+        "imageFile": "apple-cinnamon-roll.jpg"
+    },
+    "Raisin": {
+        "basePrice": 2.99,
+        "imageFile": "raisin-cinnamon-roll.jpg"
+    },
+    "Walnut": {
+        "basePrice": 3.49,
+        "imageFile": "walnut-cinnamon-roll.jpg"
+    },
+    "Double-Chocolate": {
+        "basePrice": 3.99,
+        "imageFile": "double-chocolate-cinnamon-roll.jpg"
+    },
+    "Strawberry": {
+        "basePrice": 3.99,
+        "imageFile": "strawberry-cinnamon-roll.jpg"
+    }    
+};
+
+class Roll {
+    constructor(rollType, rollGlazing, packSize, basePrice) {
+        this.type = rollType;
+        this.glazing =  rollGlazing;
+        this.size = packSize;
+        this.basePrice = basePrice;
+    }
+}
+
 let glazingOptions = [
-    {add:0},
-    {add:0},
-    {add:0.5},
-    {add:1.5},
+    {label:"Keep original", add:0},
+    {label:"Sugar milk", add:0},
+    {label:"Vanilla milk", add:0.5},
+    {label:"Double Chocolate", add:1.5},
 ]
 
-let priceOptions = [
-    {size:1},
-    {size:3},
-    {size:5},
-    {size:10},
+let sizeOptions = [
+    {label:"1",size:1},
+    {label:"3",size:3},
+    {label:"6",size:5},
+    {label:"12",size:10},
 ]
 
 
-let basePrice = 2.49
-let glazingSelect = document.querySelector("#selection-glazing")
-console.log(glazingSelect)
-let sizeSelect = document.querySelector("#selection-packsize")
-console.log(sizeSelect)
+
 
 
 
@@ -36,8 +68,6 @@ function displayPrice(glazingToDisplay, sizeToDisplay){
 
 }
 
-
-
 function priceChange() {
     // get value of selected glazing option
     // get the index from original selection
@@ -46,22 +76,57 @@ function priceChange() {
     let glazing = document.querySelector("#selection-glazing")
     let size = document.querySelector("#selection-packsize")
     
-
-
     let glazingIndex = parseInt(glazing.value)
 
     let sizeIndex = parseInt(size.value)
 
     let glazingToDisplay = glazingOptions[glazingIndex]
-    let sizeToDisplay = priceOptions[sizeIndex]
-
-    console.log(glazingToDisplay)
-    console.log(sizeToDisplay)
+    let sizeToDisplay = sizeOptions[sizeIndex]
 
     displayPrice(glazingToDisplay,sizeToDisplay)
-    
-
   }
+
+// extracting current roll
+const queryString = window.location.search;
+const params = new URLSearchParams(queryString)
+const chosenRoll = params.get('roll')
+
+// update header
+let pageHeader = document.querySelector(".introduction")
+pageHeader.innerText = chosenRoll + " Cinnamon Roll"
+
+// update product image
+let pageImg = document.querySelector('.page-image')
+let imgPath = "assets/products/"+rollsData[chosenRoll].imageFile
+pageImg.src=imgPath
+
+// update Price
+let priceShown=document.querySelector('.price')
+let basePrice = rollsData[chosenRoll].basePrice
+priceShown.innerText="$"+basePrice
+
+const cart=[]
+
+
+function addCart(){
+    let glazingSelect = document.querySelector("#selection-glazing")
+    let sizeSelect = document.querySelector("#selection-packsize")
+
+    let selectedGlazing = glazingOptions[glazingSelect.selectedIndex].label
+    let selectedPackSize = sizeOptions[sizeSelect.selectedIndex].label
+    
+    // let selectedGlazing = glazingSelect.value
+    // let selectedPackSize = sizeSelect.value
+
+    const newRoll = new Roll(chosenRoll,selectedGlazing,selectedPackSize,basePrice)
+
+    cart.push(newRoll)
+
+    console.log("cart:",cart)
+}
+
+
+
 
 
 
