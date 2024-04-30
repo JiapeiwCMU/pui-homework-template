bd1d303be70ad7f8d9b16e75d6b355298c18395e
JiapeiwCMU <jiapeiw@andrew.cmu.edu>
2024-04-29 20:56:26 -0400

    Update script-detail.js

--- Final Project/script-detail.js
+++ Final Project/script-detail.js
@@ -1,21 +1,43 @@
-// import Swup from 'https://unpkg.com/swup@4?module';
-// import SwupParallelPlugin from 'https://unpkg.com/@swup/parallel-plugin@0?module';
+import Swup from 'https://unpkg.com/swup@4?module';
+import SwupParallelPlugin from 'https://unpkg.com/@swup/parallel-plugin@0?module';
 
+// setup transition
+// https://replit.com/@philippdaun/Swup-Demo-Reveal#swup.js
+// const swup = new Swup({
+//   containers: ["#swup"],
+//   plugins: [new SwupParallelPlugin()]
+// });
+
+// swup.hooks.on('visit:start', (context) => {
+//   let x = 0.5;
+//   let y = 0.5;
+//   const event = context.trigger.event;
+//   if (event && typeof event.clientX === 'number') {
+//     x = event.clientX / window.innerWidth;
+//     y = event.clientY / window.innerHeight;
+//   }
+//   document.documentElement.style.setProperty('--click-x', x);
+//   document.documentElement.style.setProperty('--click-y', y);
+// });
 
 function makeAlbumPaths(albumCount, totalImages) {
-    const albums = {};
-    for (let i=1; i<=albumCount; i++) {
-      const albumNumber = `album${i}`;
-      const imagePaths = [];
-      for (let img = 1; img <= totalImages; img++) {
-        let thisImage = "resources/"+albumNumber+"/"+img+".JPG"
-        imagePaths.push(thisImage)
-      }
-      albums[albumNumber] = imagePaths;
+  const albums = {};
+  for (let i=1; i<=albumCount; i++) {
+    const albumNumber = `album${i}`;
+    const imagePaths = [];
+    for (let img = 1; img <= totalImages; img++) {
+      let thisImage = "resources/"+albumNumber+"/"+img+".JPG"
+      imagePaths.push(thisImage)
     }
-    return albums;
+    albums[albumNumber] = imagePaths;
   }
+  return albums;
+}
 
+const mainPage=document.querySelector(".main-page")
+mainPage.addEventListener('click',function(){
+  window.location.href = this.getAttribute('data-url');
+})
 
 // Make the albums using the function
 const albums = makeAlbumPaths(7, 12);
@@ -54,3 +76,18 @@ let currentImages = albums[currentAlbum]
 const albumHTML = generateAlbumHTML(currentImages);
 const photoDisplay = document.querySelector('.photo-display');
 photoDisplay.appendChild(albumHTML);
+
+// Displaying the popup image when clicking onto one page, and close when clicking the X
+let imgContainer = document.querySelectorAll('.container img')
+console.log("container:",imgContainer)
+imgContainer.forEach(image =>{
+  image.onclick = () =>{
+    document.querySelector('.popup-img').style.display='block';
+    document.querySelector('.popup-img img').src = image.getAttribute('src');
+
+  }
+  document.querySelector('.popup-img span').onclick = () =>{
+    document.querySelector('.popup-img').style.display='none';
+  }
+
+})
